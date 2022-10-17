fd186019c2a749c5ef89a2d654ad65c02eedd072
burakkara999 <63243552+burakkara999@users.noreply.github.com>
2022-10-17 20:23:21 +0300

    WriteOperations class is added

--- src/App.java
+++ src/App.java
@@ -81,93 +81,17 @@ public class App {
         String filePathTimeMatrix =  "./outputs/TimeMatrix.csv";
         String filePathRoutes =  "./outputs/Routes.csv";
         String filePathSolution =  "./outputs/Solution.csv";
+        String filePathClusterAnalysis =  "./outputs/Cluster_analysis.csv";
 
-        writeSolutionData(sList, filePathSolution);
-        //writePointData(heuristic1.limitedClustering(4,12), filePathLimited);
-        //writePointData(heuristic1.unlimitedClustering(), filePathUnlimited);
-       // writeTimeMatrix(maps.getTimeMatrix(), filePathTimeMatrix);
+        WriteOperations write = new WriteOperations();
+        write.writeSolutionData(sList, filePathSolution);
+        //write.writePointData(heuristic1.limitedClustering(4,12), filePathLimited);
+        //write.writePointData(heuristic1.unlimitedClustering(), filePathUnlimited);
+       // write.writeTimeMatrix(maps.getTimeMatrix(), filePathTimeMatrix);
         //writeRoutes(maps.getTimeMatrix(), filePathTimeMatrix);
     }
 
-    private static void writeTimeMatrix(float[][] timeMatrix, String filePath) {
-        try {
-            FileWriter fw = new FileWriter(filePath,true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(bw);
 
-            for(int i = 0; i < timeMatrix.length; i++) {
-                for(int j = 0; j < timeMatrix[i].length; j++) {
-                    pw.print(timeMatrix[i][j]);
-                    pw.print(",");
-                }
-                pw.println();
-            }
-
-            pw.flush();
-            pw.close();
-
-        } catch (Exception E) {
-
-        }
-    }
-
-    private static void writePointData(ArrayList<ArrayList<Point>> cList, String filePath) {
-        try {
-            FileWriter fw = new FileWriter(filePath,true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(bw);
-
-            for(int i = 0; i < cList.size(); i++) {
-                for(int j = 0; j < cList.get(i).size(); j++) {
-                    pw.print(i);
-                    pw.print(",");
-                    pw.print(cList.get(i).get(j).getX());
-                    pw.print(",");
-                    pw.print(cList.get(i).get(j).getY());
-                    pw.print(",");
-                    pw.print((int)cList.get(i).get(j).getID());
-                    pw.println();
-                }
-            }
-
-            pw.flush();
-            pw.close();
-
-        } catch (Exception E) {
-
-        }
-    }
-
-    private static void writeSolutionData(ArrayList<ArrayList<ArrayList<Point>>> sList, String filePath) {
-        try {
-            FileWriter fw = new FileWriter(filePath,true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(bw);
-
-            for(int i = 0; i < sList.size(); i++) {
-                for(int j = 0; j < sList.get(i).size(); j++) {
-                    for(int k = 0; k<sList.get(i).get(j).size();k++){
-                        pw.print(i);
-                        pw.print(",");
-                        pw.print(j);
-                        pw.print(",");
-                        pw.print(sList.get(i).get(j).get(k).getX());
-                        pw.print(",");
-                        pw.print(sList.get(i).get(j).get(k).getY());
-                        pw.print(",");
-                        pw.print((int)sList.get(i).get(j).get(k).getID());
-                        pw.println();
-                    }
-                }
-            }
-
-            pw.flush();
-            pw.close();
-
-        } catch (Exception E) {
-
-        }
-    }
 
     private static Data readData() {
         int n=40;
@@ -299,7 +223,5 @@ public class App {
         }
         return data;
     }
-
-
 
 }
